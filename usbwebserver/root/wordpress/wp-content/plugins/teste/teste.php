1d912c6226b9874e7e5463314f15d91bd2600035
Raylla Silva <raylla.l.silva@gmail.com>
2022-08-03 18:58:09 -0300

    ok

--- usbwebserver/root/wordpress/wp-content/plugins/teste/teste.php
+++ usbwebserver/root/wordpress/wp-content/plugins/teste/teste.php
@@ -2,11 +2,11 @@
    /*
 	*	Plugin Name: teste
 	*	Plugin URL: https://example.com/plugins/the-basics/
-	*	Description: sei lá plugin aleatório
+	*	Description: Sim
 	*	Version: 1.0.0
 	*	Requires at least: 5.2 
 	*	Requires PHP: 7.2
-	*	Author: Nardola
+	*	Author: Raylla
 	*	Author URI: https://author.example.com
 	*/
 
